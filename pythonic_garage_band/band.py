e95c91792b2b7d816a23a87c3603215081a51cd6
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-06-18 11:50:14 -0500

    all test passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -18,56 +18,77 @@ class Band:
             solos.append(player.play_solo())
         return solos
 
+    # def play_solos(self):
+    #     return [member.play_solo() for member in self.members]
+
+
     @classmethod
     def to_list(cls):
         return cls.instances
 
-
 class Musician:
 
-    def __init__ (self, name, type, solo):
+    def __init__ (self, name, type, instrument, solo):
         self.name = name
         self.type = type
-        self.solo = solo 
+        self.instrument = instrument 
+        self.solo = solo
 
-
-
-class Guitarist(Band):
     def __str__ (self):
-        return f'My name is {self.name} and I play guitar'
+        return f'My name is {self.name} and I play {self.instrument}'
+
     def __repr__ (self):
-        return f'Guitarist instance. Name = {self.name}'
+        return f'{self.type} instance. Name = {self.name}'
+
+    def get_instrument(self):
+        return f'{self.instrument}'
+
     def play_solo(self):
         return f'{self.solo}'
+
+class Guitarist(Musician):
+    def __init__ (self, name):
+        super().__init__(name, 'Guitarist', 'guitar', 'face melting guitar solo')
+    # def __str__ (self):
+    #     return f'My name is {self.name} and I play guitar'
+    # def __repr__ (self):
+    #     return f'Guitarist instance. Name = {self.name}'
+    # def play_solo(self):
+    #     return "face melting guitar solo"
+    # @staticmethod
+    # def get_instrument():
+    #     return "guitar"
     
-    @staticmethod
-    def get_instrument():
-        return "guitar"
 
 
-class Bassist(Band):
 
-    def __str__ (self):
-        return f'My name is {self.name} and I play bass'
-    def __repr__ (self):
-        return f'Bassist instance. Name = {self.name}'
-    def play_solo(self):
-        return f'{self.solo}'
-    
-    @staticmethod
-    def get_instrument():
-        return "bass"
+class Bassist(Musician):
+    def __init__ (self, name):
+        super().__init__(name, 'Bassist', 'bass', 'bom bom buh bom')
+    # def __str__ (self):
+    #     return f'My name is {self.name} and I play bass'
+    # def __repr__ (self):
+    #     return f'Bassist instance. Name = {self.name}'
+    # def play_solo(self):
+    #     return "bom bom buh bom"
+    # def members(self):
+    #     return "Krist Novoselic"
+    # @staticmethod
+    # def get_instrument():
+    #     return "bass"
 
-class Drummer(Band):
-
-    def __str__ (self):
-        return f'My name is {self.name} and I play drums'
-    def __repr__ (self):
-        return f'Drummer instance. Name = {self.name}'
-    def play_solo(self):
-        return f'{self.solo}'
-
-    @staticmethod
-    def get_instrument():
-        return "drums"
+class Drummer(Musician):
+    def __init__ (self, name):
+        super().__init__(name, 'Drummer', 'drums', 'rattle boom crash')
+    # def __str__ (self):
+    #     return f'My name is {self.name} and I play drums'
+    # def __repr__ (self):
+    #     return f'Drummer instance. Name = {self.name}'
+    # def play_solo(self):
+    #     return "rattle boom crash"
+    # def members(self):
+    #     return "Dave Ghrol"
+    # @staticmethod
+    # def get_instrument():
+    #     return "drums"
 
